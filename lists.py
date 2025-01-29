339972a242cad422c3e1b620e4f582f239753c4d
Saranjith <saranvlmna@gmail.com>
2025-01-29 15:37:41 +0530

    list updates

--- lists.py
+++ lists.py
@@ -9,3 +9,19 @@ print(sample[1:2])# 2nd to 3rd
 sample[1]="banana"
 
 print(sample)
+
+sample.extend(["cherry", 6])
+sample.append("dragonfruit")
+sample.insert(0,"blueberry")
+sample.remove("first")
+print(sample)
+sample.pop()
+print(sample)
+# print(sample.sort()) only available for same data types 
+print(sample.count("banana"))
+print(sample.index("banana"))
+print(sample.reverse())
+sample2=sample.copy()
+print(sample2)
+sample.clear()
+print(sample)
