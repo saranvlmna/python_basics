30a90c00d1790c1c0f9cc753dc77f7e49a966a4c
Saranjith <saranvlmna@gmail.com>
2025-02-03 17:08:41 +0530

    gues game

--- guess.game.py
+++ guess.game.py
@@ -1,14 +1,20 @@
-secreat_word= "hey"
+secret_word = "hey"
 guess = ""
-num_of_try=0
+num_of_try = 0
 
-while guess != secreat_word:
-      guess = input("Enter guess: ")
-      if(num_of_try==3):
-       print("You lose!")
-       break
-      if(guess == secreat_word):
-       print("You Win!")
-       break
-      num_of_try +=1
-      
+while guess != secret_word:
+    guess = input("Enter guess: ")
+    
+    if guess == secret_word:
+        print("You Win!")
+        break
+    
+    num_of_try += 1
+    
+    if num_of_try == 3:
+        retry = input("Need more tries? (yes/no): ")
+        if retry.lower() == "yes":
+            num_of_try = 0
+        else:
+            print("You lose!")
+            break
