de10a80d1b70a540b5a94b87ac38999341468b8c
Saranjith <saranvlmna@gmail.com>
2025-02-03 17:08:51 +0530

    transalate

---
+++ transalate.py
@@ -0,0 +1,11 @@
+vouvels=['a','e','i','o','u']
+def translate(text):
+    result =""
+    for letter in text:
+        if letter in 'AEIOUaeiou':
+            result=result+"z"
+        else:
+            result=result+letter
+    return result
+
+print(translate(input("Input a text ")))
